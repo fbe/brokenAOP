b165165d71e020e45482e1f93608b4fff9f6a301
Felix Becker <becker@Felixs-MacBook-Pro.local>
2013-05-14 11:47:19 +0200

    zwischenstand

--- src/main/java/name/felixbecker/brokenaop/configuration/AppConfig.java
+++ src/main/java/name/felixbecker/brokenaop/configuration/AppConfig.java
@@ -4,8 +4,10 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -18,7 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
-@ComponentScan("name.felixbecker.brokenaop")
+@ComponentScan(basePackages={"name.felixbecker.brokenaop"}, excludeFilters={@Filter(type=FilterType.ASSIGNABLE_TYPE, value={WorkingConfig.class, BrokenConfig.class})})
 public class AppConfig {
 	
 	@Bean
--- src/main/java/name/felixbecker/brokenaop/configuration/BrokenConfig.java
+++ src/main/java/name/felixbecker/brokenaop/configuration/BrokenConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(WorkingAspect.class)
+@Import(AppConfig.class)
 public class BrokenConfig {
 	
 	@Bean
